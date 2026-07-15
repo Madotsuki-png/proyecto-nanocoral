8b4beab9afefbfbe73a99a31cf42008e1bad8243
Madotsuki-png <262271102@alumnos.utn.edu.mx>
2026-07-15 02:21:39 -0600

    correccion dbname

--- backend/server.js
+++ backend/server.js
@@ -59,8 +59,8 @@ const dbConfig = {
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'nanocoral',
-  port: process.env.DB_PORT || 3306,
+  database: process.env.DB_NAME || 'railway',
+  port: parseInt(process.env.DB_PORT) || 3306, // Convertimos a número
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
